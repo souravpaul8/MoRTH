ffe96bb8a918a431a0dcbb85ebdb2b82826b7efe
ankitkr602 <ankitkr602@gmail.com>
2019-03-02 18:54:17 +0530

    startlocation and endlocation spinner Added

--- app/src/main/java/com/thecombatant/morth/EnterTenderDetailsActivity.java
+++ app/src/main/java/com/thecombatant/morth/EnterTenderDetailsActivity.java
@@ -33,6 +33,7 @@ public class EnterTenderDetailsActivity extends AppCompatActivity {
     String ProjectStart;
     String ProjectEnd;
 
+
     DatePickerDialog.OnDateSetListener startDateProject;
     DatePickerDialog.OnDateSetListener endDateProject;
 
@@ -52,7 +53,200 @@ public class EnterTenderDetailsActivity extends AppCompatActivity {
         tenderIdTextView.setText(tender);
 
 
+        //Adding state spinner
+        Spinner stateStart = findViewById(R.id.spinnerStatestart);
+        Spinner stateEnd = findViewById(R.id.spinnerStateEnd);
 
+        ArrayAdapter<CharSequence> ada = ArrayAdapter.createFromResource(this, R.array.india_states, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> ada0 = ArrayAdapter.createFromResource(this, R.array.jharkhand, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> ada1 = ArrayAdapter.createFromResource(this, R.array.odisha, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> ada2 = ArrayAdapter.createFromResource(this, R.array.bihar, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> ada3 = ArrayAdapter.createFromResource(this, R.array.rajasthan, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> ada4 = ArrayAdapter.createFromResource(this, R.array.tamilnadu, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> ad5 = ArrayAdapter.createFromResource(this, R.array.eastSinghbhum, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> ad6 = ArrayAdapter.createFromResource(this, R.array.westSinghbhum, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> ad7 = ArrayAdapter.createFromResource(this, R.array.Khordha, android.R.layout.simple_spinner_item);
+        final ArrayAdapter<CharSequence> ad8 = ArrayAdapter.createFromResource(this, R.array.Cuttack, android.R.layout.simple_spinner_item);
+        ada.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        ada0.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        ada1.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        ada2.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        ada3.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        ada4.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        ad5.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        ad6.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        ad7.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        ad8.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        stateStart.setAdapter(ada);
+        stateEnd.setAdapter(ada);
+        stateStart.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                // Spinner s =  findViewById(R.id.spinnerStatestart);
+                String word = adapterView.getItemAtPosition(i).toString();
+                if (word.equals(word)) {
+                    Spinner startDistrict = findViewById(R.id.spinnerDistrictstart);
+
+                    switch (i) {
+                        case 0:
+                            startDistrict.setAdapter(ada0);
+                            startDistrict.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                @Override
+                                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                                    String districtWord = parent.getItemAtPosition(position).toString();
+                                    if (districtWord.equals(districtWord)) {
+                                        Spinner startLocality = findViewById(R.id.spinnerLocalitystart);
+                                        switch (position) {
+                                            case 0:
+                                                startLocality.setAdapter(ad5);
+                                                break;
+                                            case 1:
+                                                startLocality.setAdapter(ad6);
+                                                break;
+                                        }
+                                    }
+
+
+                                }
+
+                                @Override
+                                public void onNothingSelected(AdapterView<?> parent) {
+
+                                }
+                            });
+
+
+                            break;
+                        case 1:
+                            startDistrict.setAdapter(ada1);
+                            startDistrict.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                @Override
+                                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                                    String districtWord = parent.getItemAtPosition(position).toString();
+                                    if (districtWord.equals(districtWord)) {
+                                        Spinner startLocality = findViewById(R.id.spinnerLocalitystart);
+                                        switch (position) {
+                                            case 0:
+                                                startLocality.setAdapter(ad7);
+                                                break;
+                                            case 1:
+                                                startLocality.setAdapter(ad8);
+                                                break;
+                                        }
+                                    }
+                                }
+
+                                @Override
+                                public void onNothingSelected(AdapterView<?> parent) {
+
+                                }
+                            });
+                            break;
+                        case 2:
+                            startDistrict.setAdapter(ada2);
+                            break;
+                        case 3:
+                            startDistrict.setAdapter(ada3);
+                            break;
+                        case 4:
+                            startDistrict.setAdapter(ada4);
+                            break;
+
+
+                    }
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        stateEnd.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent1, View view, int position, long id) {
+                String word1 = parent1.getItemAtPosition(position).toString();
+                if (word1.equals(word1)) {
+                    Spinner endDistrict = findViewById(R.id.spinnerDistrictEnd);
+                    switch (position) {
+                        case 0:
+                            endDistrict.setAdapter(ada0);
+                            endDistrict.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                @Override
+                                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                    String districtWord = parent.getItemAtPosition(position).toString();
+                                    if (districtWord.equals(districtWord)) {
+                                        Spinner endLocality = findViewById(R.id.spinnerLocalityEnd);
+                                        switch (position) {
+                                            case 0:
+                                                endLocality.setAdapter(ad5);
+                                                break;
+                                            case 1:
+                                                endLocality.setAdapter(ad6);
+                                                break;
+                                        }
+                                    }
+
+                                }
+
+                                @Override
+                                public void onNothingSelected(AdapterView<?> parent) {
+
+                                }
+                            });
+                            break;
+                        case 1:
+                            endDistrict.setAdapter(ada1);
+                            endDistrict.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                @Override
+                                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                    String districtWord = parent.getItemAtPosition(position).toString();
+                                    if (districtWord.equals(districtWord)) {
+                                        Spinner endLocality = findViewById(R.id.spinnerLocalityEnd);
+                                        switch (position) {
+                                            case 0:
+                                                endLocality.setAdapter(ad7);
+                                                break;
+                                            case 1:
+                                                endLocality.setAdapter(ad8);
+                                                break;
+                                        }
+                                    }
+
+                                }
+
+                                @Override
+                                public void onNothingSelected(AdapterView<?> parent) {
+
+                                }
+                            });
+                            break;
+                        case 2:
+                            endDistrict.setAdapter(ada2);
+                            break;
+                        case 3:
+                            endDistrict.setAdapter(ada3);
+                            break;
+                        case 4:
+                            endDistrict.setAdapter(ada4);
+                            break;
+
+
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+//
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
