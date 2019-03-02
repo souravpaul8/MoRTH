e7039469d58a717830bea4e889a61fd9ff4c8fa3
RAJ SINHA <raajsinha7797@gmail.com>
2019-03-03 00:37:16 +0530

    Navigation Drawer added

--- app/src/main/java/com/thecombatant/morth/HomeActivity.java
+++ app/src/main/java/com/thecombatant/morth/HomeActivity.java
@@ -1,21 +1,40 @@
 package com.thecombatant.morth;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     Button WeatherRelatedHindrances;
     Button ViewSavedData;
+
+    //for DrawerLayout
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //for DrawerLayout
+        mDrawerLayout = findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = findViewById(R.id.navigationMenu);
+        navigationView.setNavigationItemSelectedListener(this);
 
         final String tender = getIntent().getStringExtra("Tender_01");
         final String startdateofProject = getIntent().getStringExtra("startdateProject");
@@ -49,5 +68,19 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return true;
     }
 }
